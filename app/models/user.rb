1fcd15d1536abe6f8b51164de07606eb0efde5a2
ericash0923 <eric@gaglani.com>
2017-06-22 20:19:14 +0000

    associate listing with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
   validates :name, presence: true
+  
+  ##dependent: :destroy => if the users is deleted, then the listing will auto be deleted as well
+  has_many :listings, dependent: :destroy
 end
