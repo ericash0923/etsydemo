88728338229a652aeb0720e8146a771151b1c227
ericash0923 <eric@gaglani.com>
2017-06-24 21:13:09 +0000

    add seller page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,12 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
-
+  
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
@@ -62,6 +62,10 @@ class ListingsController < ApplicationController
       format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+  
+  def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
   end
 
   private
